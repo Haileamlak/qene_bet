5f886033fb29325f6eea90dcbf4d28c38c07bed3
Your Name <you@example.com>
2023-11-05 09:57:47 -0800

    Commit qene bet 3

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:qene_bet/routes/word_list.dart';
 
 void main() {
+  GoogleFonts.config.allowRuntimeFetching = false;
   runApp(const MyApp());
 }
 
@@ -13,12 +15,25 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Qene Bet',
+      title: 'ቅኔ ቤት',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.lightBlue),
         useMaterial3: true,
+        textTheme: TextTheme(
+          titleSmall: GoogleFonts.abyssinicaSil(),
+          titleMedium: GoogleFonts.abyssinicaSil(),
+          titleLarge: GoogleFonts.abyssinicaSil(),
+        ),
+        listTileTheme: ListTileThemeData(
+          tileColor: Colors.white,
+          titleTextStyle:
+              GoogleFonts.abyssinicaSil(fontSize: 16, color: Colors.lightBlue),
+          subtitleTextStyle: GoogleFonts.abyssinicaSil(color: Colors.black45),
+          leadingAndTrailingTextStyle:
+              GoogleFonts.abyssinicaSil(color: Colors.grey),
+        ),
       ),
-      home: const MyHomePage(title: 'Qene Bet'),
+      home: const MyHomePage(title: 'ቅኔ ቤት'),
     );
   }
 }
@@ -33,25 +48,20 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.white,
-        title: Text(widget.title,style: const TextStyle(color: Colors.lightBlue),),
+        title: Text(
+          widget.title,
+          style: const TextStyle(color: Colors.lightBlue),
+        ),
       ),
       drawer: const Drawer(),
-      body:const Center(
+      body: const Center(
         child: WordList(),
-      )// This trailing comma makes auto-formatting nicer for build methods.
+      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
--- lib/routes/word_list.dart
+++ lib/routes/word_list.dart
@@ -1,5 +1,9 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart' show rootBundle;
+import 'package:google_fonts/google_fonts.dart';
+import 'package:qene_bet/models/verb.dart';
 
 class WordList extends StatefulWidget {
   const WordList({super.key});
@@ -8,15 +12,25 @@ class WordList extends StatefulWidget {
   State<WordList> createState() => _WordListState();
 }
 
-
 class _WordListState extends State<WordList> {
+  List<Verb> verbs = [];
+
   @override
   void initState() {
     super.initState();
+    getWords();
+  }
+
+  void getWords() async {
+    final words = await loadAsset();
+
+    List<dynamic> jsonWords = jsonDecode(words);
+    verbs = jsonWords.map((value) => Verb.fromJson(value)).toList();
+    setState(() {});
   }
 
   Future<String> loadAsset() async {
-    return await rootBundle.loadString('assets/my_text.txt');
+    return await rootBundle.loadString('lib/assets/word_list.json');
   }
 
   @override
@@ -24,19 +38,29 @@ class _WordListState extends State<WordList> {
     return ListView.builder(
       itemBuilder: (context, index) {
         return Container(
-          margin: const EdgeInsets.only(left: 5),
+          margin: const EdgeInsets.only(left: 10, right: 5),
           decoration: const BoxDecoration(
-              border: Border(bottom: BorderSide(width: 0.1))),
-          child: const ListTile(
-            tileColor: Colors.white,
-            key: Key("key"),
-            title: Text("ቀተለ"),
-            subtitle: Text("ገደለ"),
-            trailing: Text("ስም"),
+              border: Border(bottom: BorderSide(width: 0.05))),
+          child: ListTile(
+            key: Key("$index"),
+            title: RichText(
+              text: TextSpan(
+                text: verbs[index].word,
+                style: GoogleFonts.abyssinicaSil(fontSize: 16,color: Colors.lightBlue),
+                children: [
+                  TextSpan(
+                    text: ' (${verbs[index].family})',
+                    style:  GoogleFonts.abyssinicaSil(fontSize: 10, color: Colors.black45),
+                  ),
+                ],
+              ),
+            ),
+            subtitle: Text(verbs[index].inAmharic),
+            trailing: Text(verbs[index].partOfSpeech),
           ),
         );
       },
-      itemCount: 15,
+      itemCount: verbs.length,
     );
   }
 }
